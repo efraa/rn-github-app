fb0a350900a5ab0da90741507becd897eb0b512c
Efra <efra@MacBook-Pro-de-Efra.local>
2021-03-19 21:48:24 -0400

    feat(device): add device provider

---
+++ src/providers/device/index.ts
@@ -0,0 +1,1 @@
+export { DeviceProvider, DeviceContext } from './device'
--- src/providers/styles/theme.tsx
+++ src/providers/styles/theme.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, FC } from 'react'
+import React, { ReactNode, FC, useMemo } from 'react'
 import { useFonts } from 'expo-font'
 import { fonts } from 'src/resources'
 import { ThemeProvider } from 'styled-components/native'
@@ -7,9 +7,10 @@ import { Spinner } from 'src/components/spinner'
 
 export const StyledProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [fontsLoaded] = useFonts(fonts())
+  const theme = useMemo(() => stylesGuide, [])
 
   return fontsLoaded ? (
-    <ThemeProvider theme={stylesGuide} {...{ children }} />
+    <ThemeProvider theme={theme} {...{ children }} />
   ) : (
     <Spinner />
   )
