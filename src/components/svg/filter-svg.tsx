4efb2f975c0ca97ff4829e79296ef1f8dae34e89
Efra <efra@MacBook-Pro-de-Efra.local>
2021-03-21 01:47:52 -0400

    feat: add svg component

---
+++ src/components/svg/filter-svg.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Svg, { Path } from 'react-native-svg'
+
+export const FilterSVG = () => (
+  <Svg viewBox="0 0 17.817 10.573">
+    <Path
+      d="M11.429,19.573h3.959V17.81H11.429ZM4.5,9v1.762H22.317V9Zm2.969,6.167H19.347V13.405H7.469Z"
+      transform="translate(-4.5 -9)"
+      fill="#15202b"
+    />
+  </Svg>
+)
